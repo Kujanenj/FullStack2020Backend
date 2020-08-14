91fd4a4069ba8484bbef4fde3c3ac10edc4b4aee
kujanenj <jussi.kujanen@gmail.com>
2020-08-14 13:06:37 +0300

    Post req works using mongo db

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const originalSend = app.response.send;
 const originalJson = app.response.json;
-const cors = require('cors')
+const cors = require("cors");
+const Contact = require("./models/contact");
 
-app.use(express.static('build'))
+app.use(express.static("build"));
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.response.send = function sendOverWrite(body) {
   originalSend.call(this, body);
@@ -27,25 +30,7 @@ app.use(
   })
 );
 
-morgan.token('responseData', (_req, res) => JSON.stringify(res.__custombody__));
-
-let persons = [
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2,
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3,
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4,
-  },
-];
+morgan.token("responseData", (_req, res) => JSON.stringify(res.__custombody__));
 function countPersons() {
   return persons.length;
 }
@@ -53,7 +38,8 @@ function getDate() {
   return Date();
 }
 const generateId = () => {
-  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
+  const maxId =
+    Contact.find({}).length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
   return maxId + 1;
 };
 app.get("/", (req, res) => {
@@ -64,8 +50,10 @@ app.get("/info", (req, res) => {
     `<p> PhoneBook has info of ${countPersons()} people</p> <p> ${getDate()}`
   );
 });
-app.get("/api/persons", (req, res) => {
-  res.json(persons);
+app.get("/api/persons", (request, response) => {
+  Contact.find({}).then((contacts) => {
+    response.json(contacts.map((contact) => contact.toJSON()));
+  });
 });
 app.get("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id);
@@ -93,21 +81,23 @@ app.post("/api/persons", (request, response) => {
       error: "name missing or number missing",
     });
   }
-  if (persons.find((person) => person.name == body.name)) {
+
+ /* if (Contact.find((contact) => contact.name === body.name)) {
     return response.status(400).json({
       error: "Name must be unique",
     });
-  }
-  const newPerson = {
+  }*/
+  
+  const newContact = new Contact({
     name: body.name,
     number: body.number,
     date: getDate(),
     id: generateId(),
-  };
+  });
 
-  persons = persons.concat(newPerson);
-
-  response.json(newPerson);
+  newContact.save().then((savedContact) => {
+    response.json(savedContact);
+  });
 });
 app.use(
   morgan(function (tokens, req, res) {
@@ -124,7 +114,7 @@ app.use(
   })
 );
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
