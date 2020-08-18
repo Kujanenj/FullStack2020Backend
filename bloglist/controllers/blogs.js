72e18f41a6d852c8e40939a0c51e43ab0c832d73
kujanenj <jussi.kujanen@gmail.com>
2020-08-18 11:49:33 +0300

    del put with async

--- bloglist/controllers/blogs.js
+++ bloglist/controllers/blogs.js
@@ -1,26 +1,33 @@
 const blogsRouter = require("express").Router()
 const Blog = require("../models/blog")
 
-blogsRouter.get("/", (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post("/", (request, response,next) => {
+blogsRouter.post("/", async (request, response,next) => {
   const blog = new Blog(request.body)
-  console.log("")
-  console.log(blog.title)
-
-  console.log("yes title")
   blog
     .save()
     .then(result => {
       response.status(201).json(result)
     }).catch(error => next(error))
 
+})
+blogsRouter.delete("/:id", async (req,res) => {
+  await Blog.deleteOne({ _id: req.params.id })
+  res.status(204).end()
+})
+blogsRouter.put("/:id", async (req,res) => {
+  const newBlog = {
+    title: req.body.title,
+    author: req.body.author,
+    url: req.body.url,
+    likes: req.body.likes
+  }
+  await Blog.updateOne({ _id : req.params.id },newBlog)
+  res.status(200).json(newBlog)
 })
 
 module.exports = blogsRouter
