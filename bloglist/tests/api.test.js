befdb4838544426d2a17c0492403baf3adf32a90
kujanenj <jussi.kujanen@gmail.com>
2020-08-19 16:28:20 +0300

    Part 4 complete

--- bloglist/tests/api.test.js
+++ bloglist/tests/api.test.js
@@ -2,37 +2,57 @@ const mongoose = require("mongoose")
 const supertest = require("supertest")
 const app = require("../app")
 const Blog = require("../models/blog")
+const User = require("../models/user")
 const helper = require("./test_helper")
 const api = supertest(app)
 const logger = require("../utils/logger")
+const jwt = require("jsonwebtoken")
+
+
+let userForToken = {
+  username: "user.username",
+  id: "empty",
+}
+
+let testToken // = jwt.sign(userForToken, process.env.SECRET)
+
+
+
+
 
 beforeEach(async() => {
 
   await Blog.deleteMany({})
   await Blog.insertMany(helper.initialBlogs)
-
-
-
+  await User.deleteMany({})
+  await User.insertMany(helper.initialUsers)
+  userForToken["username"]="test"
+  const users = await helper.usersInDb()
+  userForToken["id"]=users[0].id
+  testToken = jwt.sign(userForToken, process.env.SECRET)
+  
 })
 test("simpleGetTest", async() => {
-  const response = await api.get("/api/blogs")
-  console.log(response)
+  const response =  await api.get("/api/blogs").set('Authorization',"Bearer "+testToken)
   expect(response.status).toBe(200)
   expect(response.header["content-type"]).toBe("application/json; charset=utf-8")
   expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 test("idExists" , async() => {
-  const response = await api.get("/api/blogs")
+  const response = await api.get("/api/blogs").set('Authorization',"Bearer "+testToken)
   response.body.forEach(blog => expect(blog.id).toBeDefined())
 })
-test("post",async() => {
+test("Bpost",async() => {
+  const users = await helper.usersInDb()
   const newBlog = {
     title: "new",
     author: "new",
     url: "String",
-    likes: 2
+    likes: 2,
+    user : users[0].id
   }
-  await api.post("/api/blogs").send(newBlog).expect(201).expect("Content-Type", /application\/json/)
+  const response = await api.post("/api/blogs").send(newBlog).set('Authorization',"Bearer "+testToken)
+  expect(response.status).toBe(201)
   const blogsAtEnd = await helper.blogsInDb()
   logger.info(blogsAtEnd)
   expect(blogsAtEnd.length).toBe(helper.initialBlogs.length+1)
@@ -46,7 +66,7 @@ test("noLikes",async() => {
     author: "new",
     url: "String"
   }
-  const response = await api.post("/api/blogs").send(newBlog)
+  const response = await api.post("/api/blogs").set('Authorization',"Bearer "+testToken).send(newBlog)
   expect(response.body.likes).toBe(0)
 })
 test("noUrlTitle",async() => {
@@ -54,7 +74,7 @@ test("noUrlTitle",async() => {
     author: "new",
     likes : 2
   }
-  await api.post("/api/blogs").send(newBlog).expect(400)
+  await api.post("/api/blogs").set('Authorization',"Bearer "+testToken).send(newBlog).expect(400)
 })
 
 afterAll( async() => {
--- bloglist/tests/user.test.js
+++ bloglist/tests/user.test.js
@@ -1,22 +1,32 @@
 const mongoose = require("mongoose")
 const supertest = require("supertest")
 const app = require("../app")
-const Blog = require("../models/blog")
 const User = require("../models/user")
 const helper = require("./test_helper")
 const api = supertest(app)
 const logger = require("../utils/logger")
+const jwt = require("jsonwebtoken")
+let userForToken = {
+  username: "user.username",
+  id: "empty",
+}
+
+let testToken // = jwt.sign(userForToken, process.env.SECRET)
+
 
 
 beforeEach(async() => {
 
   await User.deleteMany({})
   await User.insertMany(helper.initialUsers)
-
+  userForToken["username"]="test"
+  const users = await helper.usersInDb()
+  userForToken["id"]=users[0].id
+  testToken = jwt.sign(userForToken, process.env.SECRET)
 })
 
 test("simpleUserGetTest", async() => {
-  const response = await api.get("/api/users")
+  const response = await api.get("/api/users").set('Authorization',"Bearer "+testToken)
   logger.info(response.body)
 
   expect(response.status).toBe(200)
@@ -28,7 +38,7 @@ test("userPost",async() => {
     "name":"nim13ii",
     "password":"SuperSek13red" }
 
-  await api.post("/api/users").send(newuser).expect(201).expect("Content-Type", /application\/json/)
+  await api.post("/api/users").send(newuser).set('Authorization',"Bearer "+testToken).expect(201).expect("Content-Type", /application\/json/)
   const usersAtEnd = await helper.usersInDb()
   expect(usersAtEnd.length).toBe(helper.initialUsers.length+1)
 
@@ -37,15 +47,15 @@ test("invalidUsername",async() => {
   const newuser = { "username":"12",
     "name":"nim13ii",
     "password":"SuperSek13red" }
-  await api.post("/api/users").send(newuser).expect(400).expect("Content-Type", /application\/json/)
+  await api.post("/api/users").send(newuser).set('Authorization',"Bearer "+testToken).expect(400).expect("Content-Type", /application\/json/)
   const usersAtEnd = await helper.usersInDb()
   expect(usersAtEnd.length).toBe(helper.initialUsers.length)
 })
-test("invalidPassword",async()=>{
-    const newuser = { "username":"1233",
+test("invalidPassword",async() => {
+  const newuser = { "username":"1233",
     "name":"nim13ii",
     "password":"Su" }
-  await api.post("/api/users").send(newuser).expect(400).expect("Content-Type", /application\/json/)
+  await api.post("/api/users").send(newuser).set('Authorization',"Bearer "+testToken).expect(400).expect("Content-Type", /application\/json/)
   const usersAtEnd = await helper.usersInDb()
   expect(usersAtEnd.length).toBe(helper.initialUsers.length)
 })
