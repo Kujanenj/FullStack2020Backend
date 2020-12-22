2efc1217d7d10dc81dfc4232cc8c6839a300cba1
kujanenj <jussi.kujanen@gmail.com>
2020-12-22 14:33:21 +0200

    8.2

--- osa8/phonebook/index.js
+++ osa8/phonebook/index.js
@@ -91,16 +91,19 @@ type Book {
     id: ID!
     genres: [String]!
 }
+
   type Query {
       bookCount : Int!
       authorCount: Int!
+      allBooks: [Book!]!
   }
 `
 
 const resolvers = {
   Query: {
       bookCount: () =>books.length,
-      authorCount: ()=> authors.length
+      authorCount: ()=> authors.length,
+      allBooks: () => books
   }
 }
 
