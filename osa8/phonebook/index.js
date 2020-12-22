27c66832dcdfb77a49593193970ae2cac2ffe394
kujanenj <jussi.kujanen@gmail.com>
2020-12-22 15:35:29 +0200

    8.7

--- osa8/phonebook/index.js
+++ osa8/phonebook/index.js
@@ -111,6 +111,10 @@ type Author {
     author: String!
     genres: [String]!
     ): Book
+    editAuthor(
+        name: String!
+        setBornTo: Int!
+    ): Author
   }
 `
 
@@ -144,6 +148,15 @@ const resolvers = {
             const book = { ...args, id: uuid() }
             books = books.concat(book)
             return book
+        },
+        editAuthor: (root, args) => {
+            const author = authors.find(p => p.name === args.name)
+            if (!author) {
+                return null
+            }
+            const updatedAuthor = { ...author, born: args.setBornTo }
+            authors = authors.map(p => p.name === args.name ? updatedAuthor : p)
+            return updatedAuthor
         }
     }
 }
